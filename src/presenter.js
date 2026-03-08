e8eb798eb02baa0f814c20625e27c751861df17e
Flora <flora.ortiz@ucb.edu.bo>
2026-03-08 18:21:29 -0400

    feat: incremento html

--- src/presenter.js
+++ src/presenter.js
@@ -1,18 +1,26 @@
-import { mostrarCantidad, mostrarPrecioUnitario, mostrarPrecioNeto } from "./ventas.js";
+import { mostrarCantidad, mostrarPrecioUnitario, mostrarPrecioNeto, calcularImpuesto, mostrarImpuesto } from "./ventas.js";
 
 const cantidadInput = document.querySelector("#cantidad");
 const precioInput = document.querySelector("#precio");
+const estadoInput = document.querySelector("#estado");
 const botonTotalizar = document.querySelector("#btn-j1");
 const divResultado = document.querySelector("#resultado");
 
 botonTotalizar.addEventListener("click", () => {
     const cantidad = Number(cantidadInput.value);
     const precio = Number(precioInput.value);
+    const estado = estadoInput.value;
+    const impuesto = calcularImpuesto(precioNeto, estado);
+    const porcentaje = mostrarImpuesto(estado);
+    const total = precioNeto + impuesto;
 
     const cantidadValidada = mostrarCantidad(cantidad);
     const precioValidado = mostrarPrecioUnitario(precio);
     
     const precioNeto = mostrarPrecioNeto(cantidad, precio);
 
-    divResultado.innerHTML = `<p>Precio neto (${cantidadValidada}*$${precioValidado}): $${precioNeto}</p>`;
+    divResultado.innerHTML = `<p>Precio neto (${cantidadValidada}*$${precioValidado}): $${precioNeto}</p>
+    <p>Impuesto para ${estado} (%${porcentaje}): $${impuesto.toFixed(2)}</p>
+    <p>Precio total (+impuesto): $${total.toFixed(2)}</p>`
+    ;
 });
